6a0fdd1fd7da79b72a29cfeaefc1621bb6167b8a
Maor <maorseg@gmail.com>
2025-07-14 22:44:26 +0300

    skip tests

--- tests/ui/kidsProductsCategory.spec.ts
+++ tests/ui/kidsProductsCategory.spec.ts
@@ -2,14 +2,14 @@ import { test } from './setup';
 import { expect } from '@playwright/test';
 import { TestConfig } from '../../test.config';
 
-test('Kids - Dress Products exist', async ({ page }) => {
+test.skip('Kids - Dress Products exist', async ({ page }) => {
   await page.getByRole('link', { name: 'Products' }).click();
   await page.locator('a[href="#Kids"]').click();
   await page.getByRole('link', { name: 'Dress' }).click();
   await expect(page.getByRole('heading', { name: 'Kids - Dress Products' })).toHaveText(TestConfig.products.kidsDressProducts);
 });
 
-test('Kids - Tops & Shirts Products exist', async ({ page }) => {
+test.skip('Kids - Tops & Shirts Products exist', async ({ page }) => {
   await page.getByRole('link', { name: 'Products' }).click();
   await page.locator('a[href="#Kids"]').click();
   await page.getByRole('link', { name: 'TOPS & SHIRTS' }).click();
